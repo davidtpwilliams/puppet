847213c4083980dd2ec62b129703d103793f1819
David Williams <blakey249@gmail.com>
2015-04-19 15:44:48 +1000

    aws create lamp stack with cloud-init

---
+++ lamp/create_lamp.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# We are creating and AWS EC2 instance that is a LAMP instance
+# Linux Apache Mysql Php
+# We also want to keep a record of the InstanceId in a fil for tagging
+# Also for testing if we want top delete the new instance then 
+# we can run the "terminate" which reads the file instance. 
+
+if [ $# -eq 0 ]
+then
+   echo "ERROR: Wrong number of parameters!"
+   echo "Usage: crlamp <number>"
+   exit
+fi
+
+# First create lamp server then tag
+# The lamp.data file is in #!bin/sh format
+./cloud-config "$(< lamp.data)" |grep InstanceId | sed -e 's/[:," ]//g' -e 's/InstanceId//g' > instance
+./tagit "$(< instance)" $1
